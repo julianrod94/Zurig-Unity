dda9f23a51c61206b92388971208ec33afa2b7ea
jcaracciolo <jcaracciolo@itba.edu.ar>
2018-11-21 11:36:11 -0300

    New prefabs

---
+++ Assets/Scripts/Rotating.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,3*60 * Time.deltaTime,0);
+	}
+}
